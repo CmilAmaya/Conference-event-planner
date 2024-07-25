2a439e109025549b8a46a31fade975481f39140e
CmilAmaya <macamayar@gmail.com>
2024-07-24 20:48:00 -0500

    update settings for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Conference-event-planner/',
   plugins: [react()],
-  base: '/Conference-event-planner/'
 })
